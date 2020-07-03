3d99b831f924085a04ec4e9b2e2a3eb3a6a6f376
Jennifer Carrera <jennifer.s.carrera@gmail.com>
2020-07-03 14:16:56 -0400

    Added images & set backgrounds to different colors

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,10 +4,19 @@ public class Word {
 
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mImageResourceID = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceID) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceID = imageResourceID;
     }
 
     public String getMiworkTranslation() {
@@ -17,4 +26,11 @@ public class Word {
     public String getDefaultTranslation () {
         return mDefaultTranslation;
     }
+
+    public int getImageResourceID() { return mImageResourceID; }
+
+    public boolean hasImage() {
+        return mImageResourceID != NO_IMAGE_PROVIDED;
+    }
+
 }
